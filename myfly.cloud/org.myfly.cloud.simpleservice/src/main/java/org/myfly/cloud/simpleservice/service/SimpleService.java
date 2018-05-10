96af188caceb4f0012a010b2f69c9d2327bde9e3
xwh123807 <xwh123807@gmail.com>
2018-05-10 22:15:01 +0800

    调整simpleservice

--- myfly.cloud/org.myfly.cloud.simpleservice/src/main/java/org/myfly/cloud/simpleservice/service/SimpleService.java
+++ myfly.cloud/org.myfly.cloud.simpleservice/src/main/java/org/myfly/cloud/simpleservice/service/SimpleService.java
@@ -1,5 +1,19 @@
 package org.myfly.cloud.simpleservice.service;
 
-public class SimpleService {
+import java.util.List;
 
+import org.myfly.cloud.simpleservice.domain.SUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleService {
+	@Autowired
+	private UserRepostory userRepo;
+
+	@RequestMapping("list")
+	public List<SUser> list() {
+		return userRepo.findAll();
+	}
 }
