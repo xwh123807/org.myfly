e6de38232eff7cefe270d787d0944ed7eb6c57e3
xwh123807 <xwh123807@gmail.com>
2018-05-10 21:48:23 +0800

    fix configserver

--- myfly.cloud/org.myfly.cloud.configserver/src/main/java/org/myfly/configserver/ConfigServerApplication.java
+++ myfly.cloud/org.myfly.cloud.configserver/src/main/java/org/myfly/configserver/ConfigServerApplication.java
@@ -1,16 +1,14 @@
 package org.myfly.configserver;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ConfigServerApplication {
 	public static void main(String args[]) {
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(ConfigServerApplication.class);
-		builder.profiles("native").run(args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
