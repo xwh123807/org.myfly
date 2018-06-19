a8853d54d24c6f85177f2059e2e87fd3389d8d05
xwh123807 <xwh123807@gmail.com>
2018-06-19 22:59:47 +0800

    增加读取类

---
+++ org.myfly.platform.core/src/main/java/org/myfly/platform/core3/metadata/handler/DefaultValueHandler.java
@@ -0,0 +1,12 @@
+package org.myfly.platform.core3.metadata.handler;
+
+import org.myfly.platform.core3.metadata.define.IValueHandler;
+
+/**
+ * 基本数据类型字段值读取类
+ * @author xiangwanhong
+ *
+ */
+public class DefaultValueHandler implements IValueHandler{
+
+}
