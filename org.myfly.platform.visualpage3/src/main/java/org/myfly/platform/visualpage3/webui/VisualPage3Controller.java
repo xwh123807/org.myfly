a12cf1c9a3a5dc466fb6e6df5fc754d7f1ef680e
xiangwanhong <xiangwanhong@xiangwaongdembp.lan>
2018-07-10 17:50:37 +0800

    增加VUE DEMO

--- org.myfly.platform.visualpage3/src/main/java/org/myfly/platform/visualpage3/webui/VisualPage3Controller.java
+++ org.myfly.platform.visualpage3/src/main/java/org/myfly/platform/visualpage3/webui/VisualPage3Controller.java
@@ -9,6 +9,7 @@ import org.myfly.platform.core3.flydata.service.IFlyDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -47,6 +48,34 @@ public class VisualPage3Controller {
 		view.addObject("pageInfo", pageInfo);
 		view.addObject("webresPath", "/");
 		view.addObject("subPage", "tables");
+		List<FlyEntityMap> tables = dataService.findAll("ptable");
+		view.addObject("objList", tables);
+		return view;
+	}
+	
+	@GetMapping("vue")
+	public ModelAndView vue() {
+		ModelAndView view = new ModelAndView("vue-empty");
+		Map<String, String> pageInfo = new HashMap<>();
+		pageInfo.put("title", "VUE");
+		pageInfo.put("subTitle", "tables");
+		view.addObject("pageInfo", pageInfo);
+		view.addObject("webresPath", "/");
+		view.addObject("subPage", "vue-content");
+		List<FlyEntityMap> tables = dataService.findAll("ptable");
+		view.addObject("objList", tables);
+		return view;
+	}
+	
+	@GetMapping("vue/{subPage}")
+	public ModelAndView vueForSubPage(@PathVariable(name ="subPage") String subPage) {
+		ModelAndView view = new ModelAndView("vue-empty");
+		Map<String, String> pageInfo = new HashMap<>();
+		pageInfo.put("title", "VUE");
+		pageInfo.put("subTitle", "tables");
+		view.addObject("pageInfo", pageInfo);
+		view.addObject("webresPath", "/");
+		view.addObject("subPage", subPage);
 		List<FlyEntityMap> tables = dataService.findAll("ptable");
 		view.addObject("objList", tables);
 		return view;
